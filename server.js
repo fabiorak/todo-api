581c10238977ac57fa85b6e70e9cb91a630217bc
Fabio Rachella <fabio.rachella@regione.veneto.it>
2016-11-25 22:43:03 +0100

    init

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+    res.send('About Express');
+});
+app.use(express.static(__dirname + '/public'));
+app.listen(port, function () {
+    console.log('Express server partito alla porta ' + port);
+});
