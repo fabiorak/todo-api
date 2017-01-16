a29bd3f1c321ef733659deec1321c155f1bc6e55
Fabio Rachella <fabio.rachella@regione.veneto.it>
2017-01-16 10:40:27 +0100

    modificato

--- server.js
+++ server.js
@@ -2,6 +2,39 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
+var todos = [{
+    id: 1,
+    description:'Incontrare Mario a pranzo',
+    completed: false
+},{
+    id: 2,
+    description:'Andare al mercato',
+    completed: false
+},{
+    id: 3,
+    description:'Mangiare il gatto',
+    completed: true
+}];
+
+//GET todos
+app.get('/todos', function (req, res) {
+    res.json(todos);
+});
+
+//GET todos/:id
+app.get('/todos/:id', function (req, res) {
+    
+    var searchId= parseInt(req.params.id,10);
+    
+    todos.forEach(function(todo) {
+        if(todo.id===searchId)
+        {
+            res.json(todo);   
+        }
+    });
+        res.status(404).send();
+});
+
 
 app.get('/about', function (req, res) {
     res.send('About Express');
