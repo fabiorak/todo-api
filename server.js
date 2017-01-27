36cbc8b8d24ba909cb3567a31d3008ef542e1b98
Fabio Rachella <fabio.rachella@regione.veneto.it>
2017-01-28 00:13:21 +0100

    fatto delete con sequelize

--- server.js
+++ server.js
@@ -42,23 +42,7 @@ app.get('/todos', function (req, res) {
         res.status(500).send();
     });
 
-    // var filteredTodos = todos;
-    // if (queryParams.hasOwnProperty('completed')) {
-    //     if (queryParams.completed === 'false')
-    //         filteredTodos = _.where(filteredTodos, { 'completed': false });
-    //     else if (queryParams.completed === 'true')
-    //         filteredTodos = _.where(filteredTodos, { 'completed': true });
-    // }
 
-    // if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-
-    //     filteredTodos = _.filter(filteredTodos, function (todo) {
-    //         return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-    //     });
-
-    // }
-
-    // res.json(filteredTodos);
 });
 
 //GET todos/:id
@@ -132,14 +116,23 @@ app.delete('/todos/:id', function (req, res) {
 
     var searchId = parseInt(req.params.id, 10);
 
-    var todo = _.findWhere(todos, { id: searchId });
-    if (todo) {
-        todos = _.without(todos, todo);
-        res.json(todo);
-    }
-    else {
-        res.status(404).send('Nessun todo trovato con id=' + searchId);
-    }
+    db.todo.destroy({
+        where: {
+            id: searchId
+        }
+    }).then(function (rowsDeleted) {
+        if (rowsDeleted === 0) {
+            res.status(404).json({
+                error: 'Nessun todo con id'
+            });
+        } else {
+            res.status(204).send();
+        }
+    }, function (e) {
+        res.status(500).json(e);
+    });
+
+
 });
 
 
