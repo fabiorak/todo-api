a06b214cad0c0b8a1be28d77d1c32125233c81b2
Fabio Rachella <fabio.rachella@regione.veneto.it>
2017-01-27 23:50:31 +0100

    fatto get con where condition passata in querystring con sequelize

--- server.js
+++ server.js
@@ -19,24 +19,46 @@ app.all('/test', function (req, res) {
 
 //GET todos
 app.get('/todos', function (req, res) {
-    var queryParams = req.query;
-    var filteredTodos = todos;
-    if (queryParams.hasOwnProperty('completed')) {
-        if (queryParams.completed === 'false')
-            filteredTodos = _.where(filteredTodos, { 'completed': false });
-        else if (queryParams.completed === 'true')
-            filteredTodos = _.where(filteredTodos, { 'completed': true });
+    var query = req.query;
+
+    var where = {};
+
+    if (query.hasOwnProperty('completed') && query.completed === 'true') {
+        where.completed = true;
+    } else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+        where.completed = false;
     }
 
-    if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-
-        filteredTodos = _.filter(filteredTodos, function (todo) {
-            return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-        });
-
+    if (query.hasOwnProperty('q') && query.q.length > 0) {
+        where.description = {
+            $like: '%' + query.q + '%'
+        };
     }
 
-    res.json(filteredTodos);
+
+    db.todo.findAll({ where: where }).then(function (todos) {
+        res.json(todos);
+    }, function (e) {
+        res.status(500).send();
+    });
+
+    // var filteredTodos = todos;
+    // if (queryParams.hasOwnProperty('completed')) {
+    //     if (queryParams.completed === 'false')
+    //         filteredTodos = _.where(filteredTodos, { 'completed': false });
+    //     else if (queryParams.completed === 'true')
+    //         filteredTodos = _.where(filteredTodos, { 'completed': true });
+    // }
+
+    // if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+
+    //     filteredTodos = _.filter(filteredTodos, function (todo) {
+    //         return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+    //     });
+
+    // }
+
+    // res.json(filteredTodos);
 });
 
 //GET todos/:id
